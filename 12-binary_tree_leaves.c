2e0903fa07d33aecc27055e624c395e239064d2f
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-12-18 14:53:30 -0800

    count leasves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,9 +9,9 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
+		size_t leaves = 0;
 
-	if (tree)
+		if (tree)
 			{
 			leaves += (!tree->left && !tree->right) ? 1 : 0;
 			leaves += binary_tree_leaves(tree->left);
